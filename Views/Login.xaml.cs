aa89ddb7ad9f54789bfc0f450628a044aed5d3a5
illelI <ilellixd@gmail.com>
2023-06-22 18:23:42 +0200

    hehe

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -66,5 +66,21 @@ namespace CourierSystem.Views
             this.Close();
         }
 
+        private void textBox_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+           
+            var workingWidth = this.Width;
+            titleTextBlock.FontSize = Height * 0.08;
+            password.Width = workingWidth * 0.5;
+            password.FontSize = Height * 0.05;
+            password.Height = Height * 0.08;
+            Username.Width = workingWidth * 0.5;
+            Username.FontSize = Height * 0.05;
+            Username.Height = Height * 0.08;
+            LoginButton.FontSize = this.Height * 0.05;
+            ReturnButton.FontSize = this.Height * 0.05;
+            loginTextBlock.FontSize = Height * 0.05;
+            passwordTextBlock.FontSize = Height * 0.05;
+        }
     }
 }
